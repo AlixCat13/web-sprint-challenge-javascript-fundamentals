6b6d2407c51b2b2d49f8b8a5681cfd19cb2b06f7
Cody C <alix.henry@ymail.com>
2020-06-12 21:51:26 -0500

    Have Task 1 completed. Still have some work to do on Task 2

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,15 @@ myFunction();
 
 // Explanation: 
 
+// the nested function can access the variable internal because the external function is calling the internal function. The external function is outside of the scope of the internal function but is still accessible because the external function is considered a closure of the internal function. 
+
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const summation = ((number) => {
+  return number++;
+});
+
+console.log(summation(4));
